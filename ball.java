2589717f60edb19258c5ab269826e27d95e7ab19
Your Name <you@example.com>
2022-12-09 11:09:08 -0800

    ball

--- ball.java
+++ ball.java
@@ -5,8 +5,11 @@ import java.awt.Rectangle;
 import java.util.Random;
 
 public class ball {
-    int x, y, xDirection, yDirection;
-	
+    int x;
+    int y;
+    int xDirection;
+    int yDirection;
+	Rectangle r;
 	
 	// int p1score, p2score;
 	
@@ -23,7 +26,7 @@ public ball(int x, int y){
     //
     //
 
-    ball = new Rectangle(this.x, this.y, 25,25);
+   r = new Rectangle(this.x, this.y, 25,25);
 }
 public void setXDirection(int xdirect){
     xDirection = xdirect;
@@ -33,7 +36,7 @@ public void setYDirection(int ydirect){
 }
 public void draw(Graphics g){
     g.setColor(Color.WHITE);
-    g.fillRect(ball.x, ball.y, ball.width, ball.height);
+    g.fillRect(this.x, this.y, 25, 25);
 }
 public void collision(){
     // wait for paddle 1 and 2
@@ -45,8 +48,8 @@ public void collision(){
 }
 public void moveball(){
     collision();
-    ball.x += xDirection;
-    ball.y += yDirection;
+    x += xDirection;
+    y += yDirection;
 
     //if (ball.x <= # for player 1){
         //setXDirection(+1);
--- pongMain.java
+++ pongMain.java
@@ -1,7 +1,7 @@
 public class pongMain {
     public static void main(String[] args) {
         pong pg = new pong();
-        ball b = new ball(500,500);
+   
         
         
     }
